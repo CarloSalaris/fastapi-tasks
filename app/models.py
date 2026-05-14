89cab80a202ce16b658203cf49a9c16a706d6a6a
CarloSalaris <carloa.salaris@gmail.com>
2026-05-15 00:44:41 +0200

    feat: add task models

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from sqlmodel import SQLModel, Field
+
+
+class TaskBase(SQLModel):
+    title: str
+    description: str | None = None
+    completed: bool = False
+
+
+class Task(TaskBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime | None = None
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskUpdate(SQLModel):
+    title: str | None = None
+    description: str | None = None
+    completed: bool | None = None
+
+
+class TaskPublic(TaskBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime | None
